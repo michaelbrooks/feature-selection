5fce96adc09e556c5865f038f7ae53335f0aef0d
Michael Brooks <mjbrooks@uw.edu>
2015-02-24 17:42:34 -0800

    update provision for convenience

--- provision.sh
+++ provision.sh
@@ -8,7 +8,28 @@ apt-get upgrade -y
 apt-get install -y libatlas-dev libblas-dev liblapack-dev gfortran
 apt-get install -y python-numpy python-scipy
 
+VAGRANT_HOME=/home/vagrant
+VENV_NAME=feature-selection
+PROJECT_ROOT=$VAGRANT_HOME/feature-selection
+
 su --login vagrant <<EOF
-mkvirtualenv -a /home/vagrant/feature-selection -r /home/vagrant/feature-selection/requirements.txt --system-site-packages feature-selection
+mkvirtualenv -a $PROJECT_ROOT -r $PROJECT_ROOT/requirements.txt --system-site-packages $VENV_NAME
 ipython test.py
 EOF
+
+# Add the workon command to the bashrc
+echo "Augmenting user's bashrc file..."
+
+if grep -q 'workon' ${VAGRANT_HOME}/.bashrc; then
+    echo "workon already in bashrc"
+else
+    echo "workon $VENV_NAME" >> ${VAGRANT_HOME}/.bashrc
+    echo "added workon to bashrc"
+fi
+
+if grep -q 'remount' ${VAGRANT_HOME}/.bashrc; then
+    echo "remount already in bashrc"
+else
+    echo "alias remount_vagrant='sudo mount -o remount home_vagrant_textvisdrg'" >> ${VAGRANT_HOME}/.bashrc
+    echo "added remount_vagrant to bashrc"
+fi
